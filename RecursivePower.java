ed69aff6d24fc69fb329247bda048694ddf89fa3
Maheraj Uddin <maheraj.cse@gmail.com>
2018-11-29 21:27:33 +0600

    Create RecursivePower.java

---
+++ RecursivePower.java
@@ -0,0 +1,26 @@
+public class RecursivePower {
+  public static void main(String[] args) {
+    System.out.println(doPower(4, 3));
+  }
+  
+  public static boolean isEven(int n) {
+    return n % 2 == 0;
+  }
+  
+  public static boolean isOdd(int n) {
+    return !isEven(n);
+  }
+  
+  public static int doPower(int x, int n) {
+    if(n == 0) {
+      return 1;
+    } else if(n < 0) {
+      return 1/doPower(x, -n);
+    } else if(isOdd(n)) {
+      return x * doPower(x, n - 1);
+    } else {//is even
+      int val = doPower(x, n/2);
+      return val * val;
+    }
+  }
+}
