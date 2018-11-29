a65771f683fda8cab93004d30945fc29c4977bb6
Maheraj Uddin <maheraj.cse@gmail.com>
2018-11-29 20:58:06 +0600

    Create RecursionPalindrome.java

---
+++ RecursionPalindrome.java
@@ -0,0 +1,33 @@
+public class RecursionPalindrome {
+
+  public static void main(String[] Args) {
+
+        String str = "maham";
+        System.out.println(isPalindrome(str));
+
+    }
+    
+  public static char firstCharacter(String str) {
+        return str.charAt(0);
+    }
+
+    public static char lastCharacter(String str) {
+        return str.charAt(str.length() - 1);
+    }
+
+    public static String middleCharacters(String str) {
+        return str.substring(1, str.length() - 1);
+    }
+
+    public static boolean isPalindrome(String str) {
+        if (str.length() == 0 || str.length() == 1) {
+            return true;
+        }
+
+        if (firstCharacter(str) == lastCharacter(str)) {
+            return isPalindrome(middleCharacters(str));
+        } else {
+            return false;
+        }
+    }
+}
