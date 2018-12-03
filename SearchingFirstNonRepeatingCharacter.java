325390958baf60daf9cda1272c9af62c7aeb08fe
Maheraj Uddin <maheraj.cse@gmail.com>
2018-12-04 01:10:03 +0600

    Create SearchingFirstNonRepeatingCharacter.java

---
+++ SearchingFirstNonRepeatingCharacter.java
@@ -0,0 +1,34 @@
+// This question was asked in zalando tech interview test
+
+public class SearchingFirstNonRepeatingCharacter {
+    static final int NO_OF_CHARS = 256;
+    static char count[] = new char[NO_OF_CHARS];
+
+    static void getCharCountArray(String str) {
+        for (int i = 0; i < str.length(); i++)
+            count[str.charAt(i)]++;
+    }
+
+    static int firstNonRepeating(String str) {
+        getCharCountArray(str);
+        int index = -1, i;
+
+        for (i = 0; i < str.length(); i++) {
+            if (count[str.charAt(i)] == 1) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    public static void main(String[] Args) {
+        String str = "maheraj";
+        int index = firstNonRepeating(str);
+
+        System.out.println(index == -1 ? "N/A" : str.charAt(index));
+    }
+
+
+}
