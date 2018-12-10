0b9e37a58ba23dac6dc3aeaa86cd621a43bf12a4
Maheraj Uddin <maheraj.cse@gmail.com>
2018-12-10 15:27:59 +0600

    Create SearchBinarySearch.java

---
+++ SearchBinarySearch.java
@@ -0,0 +1,25 @@
+public class SearchBinarySearch {
+
+public static void main(String[] args) {
+  int[]array = {1,2,3,4,5,6,7,8,9};
+  int targetValue = 8;
+  
+  System.out.println(doSearch(array, targetValue));
+}
+
+  public static int doSearch(int[]array, int targetValue) {
+    int min = 0, max = array.length;
+    int guess;
+    whicle(min <= max) {
+      guess = (min + max) / 2;
+      if(array[guess] == targetValue) {
+        return guess;
+      } else if(array[guess] < targetValue) {
+        min = guess + 1;
+      } else {
+        max = guess - 1;
+      }
+    }
+    return -1;
+  }
+}
