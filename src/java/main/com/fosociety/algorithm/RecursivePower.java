ab32ce48d1e9be4714b9fd12e5c35ca85f7c5804
maheraj <maheraj.cse@gmail.com>
2019-04-06 23:24:43 +0600

    dow power algorithm updated

--- src/java/main/com/fosociety/algorithm/RecursivePower.java
+++ src/java/main/com/fosociety/algorithm/RecursivePower.java
@@ -1,28 +1,29 @@
 package com.fosociety.algorithm;
 
 public class RecursivePower {
-  public static void main(String[] args) {
-    System.out.println(doPower(4, 3));
-  }
-  
-  public static boolean isEven(int n) {
-    return n % 2 == 0;
-  }
-  
-  public static boolean isOdd(int n) {
-    return !isEven(n);
-  }
-  
-  public static int doPower(int x, int n) {
-    if(n == 0) {
-      return 1;
-    } else if(n < 0) {
-      return 1/doPower(x, -n);
-    } else if(isOdd(n)) {
-      return x * doPower(x, n - 1);
-    } else {//is even
-      int val = doPower(x, n/2);
-      return val * val;
+
+    public static void main(String[] args) {
+        System.out.println(doPower(4, 3));
     }
-  }
+
+    public static boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+
+    public static boolean isOdd(int n) {
+        return !isEven(n);
+    }
+
+    public static int doPower(int x, int n) {
+        if (n == 0) {
+            return 1;
+        } else if (n < 0) {
+            return 1 / doPower(x, -n);
+        } else if (isOdd(n)) {
+            return x * doPower(x, n - 1);
+        } else {//is even
+            int val = doPower(x, n / 2);
+            return val * val;
+        }
+    }
 }
